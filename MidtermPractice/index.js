e3548f2e3350a8e4d06297b63742cd8eed61e92d
tayloralton <taal2985@colorado.edu>
2021-10-28 13:26:13 -0600

    Update index.js

--- MidtermPractice/index.js
+++ MidtermPractice/index.js
@@ -10,7 +10,7 @@ for(i=0; i<document.getElementsByClassName('day').length; i++){
   document.getElementsByClassName("day")[i].addEventListener('click', setActive, false);
   // This is making it so I can click on the days because they are active (thing listening for, function triggering, want to get the event data)
   document.getElementsByClassName("day")[i].id = days[i];
-  // 21.10 in the youtube video for comment!!! Whenever I do this the days go away (adding the id so we can access it later)
+  // adding the id so we can access it later
 }
 
 document.getElementById("Sunday").classList.toggle('active');
@@ -33,7 +33,6 @@ function setActive(e){
   }
 
   currentday = document.getElementsByClassName('active')[0].id;
-  console.log(currentday);
   document.getElementById(currentday + 'tab').style.display = "block";
   document.getElementById('image').innerHTML = "<img src='assets/" + currentday.toLowerCase() + ".jpeg' >";
 
