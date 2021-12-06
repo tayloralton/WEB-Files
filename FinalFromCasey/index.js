0373d13bcbf28fc73dc0d4003231a9c8e31e72f0
tayloralton <taal2985@colorado.edu>
2021-12-05 20:13:25 -0700

    Create index.js

---
+++ FinalFromCasey/index.js
@@ -0,0 +1,10 @@
+console.log('hello!');
+
+// typing animation
+var Typed = new Typed(".typing",{
+  strings: ["Hello!", "Welcome!"],
+  typeSpeed: 80,
+  backSpeed: 100,
+  loop: true
+});
+// source: https://www.youtube.com/watch?v=tcskp-ncN0I&list=WL&index=46&t=5672s
