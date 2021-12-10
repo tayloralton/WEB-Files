9318ead97575be6635b72ab33ed72b64911eb12a
tayloralton <taal2985@colorado.edu>
2021-12-10 15:12:29 -0700

    Create index-2.js

---
+++ FinalFromCasey/index-2.js
@@ -0,0 +1,8 @@
+// typing animation
+var Typed = new Typed(".typing",{
+  strings: ["Hello!", "Welcome!", "I am glad you're here!"],
+  typeSpeed: 80,
+  backSpeed: 100,
+  loop: true
+});
+// source: https://www.youtube.com/watch?v=tcskp-ncN0I&list=WL&index=46&t=5672s
